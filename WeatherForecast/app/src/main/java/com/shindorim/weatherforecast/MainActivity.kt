57890e9133294589d8d8244accb49f85e536494c
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-05-03 22:45:45 +0900

    add fragment

--- WeatherForecast/app/src/main/java/com/shindorim/weatherforecast/MainActivity.kt
+++ WeatherForecast/app/src/main/java/com/shindorim/weatherforecast/MainActivity.kt
@@ -2,11 +2,40 @@ package com.shindorim.weatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHome = findViewById<ImageButton>(R.id.btn_main_home)
+        val btnFavorite = findViewById<ImageButton>(R.id.btn_main_favorite)
+        val btnCart = findViewById<ImageButton>(R.id.btn_main_cart)
+        val btnSetting = findViewById<ImageButton>(R.id.btn_main_setting)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment()).commit()
+
+        btnHome.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, homeFragment())
+                .commit()
+        }
+        btnFavorite.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, favoriteFragment())
+                .commit()
+        }
+        btnCart.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, cartFragment())
+                .commit()
+        }
+        btnSetting.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, settingFragment())
+                .commit()
+        }
     }
 }
