a3a1dd951b4f5cf3f86777c85e4e2fa3bc6c687c
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-05-05 23:34:36 +0900

    add search icon

---
+++ WeatherForecast/app/src/main/java/com/shindorim/weatherforecast/settingList.kt
@@ -0,0 +1,21 @@
+package com.shindorim.weatherforecast
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class settingList : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_list, container, false)
+    }
+}
